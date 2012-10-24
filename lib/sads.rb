89b84e5bb14819d34cff75ce112e4c8bff4cbe83
Chris Imbriano <Christopher.Imbriano@gmail.com>
2012-10-24 13:13:19 -0400

    Adding method stubs

--- lib/sads.rb
+++ lib/sads.rb
@@ -2,7 +2,27 @@
 
 class Sads
 	
-	attr_accessor :labels
+	attr_accessor :labels, :leaves, :digest
 	attr_reader :L, :R, :pubkey
+
+	def initialize()
+		# Size of Universe
+		# 
+
+
+	end
+
+
+	def addElement(ele)
+
+	end
+
+	def removeElement(ele)
+
+	end
+
+	def exists?(ele)
+		return false
+	end
 
 end
