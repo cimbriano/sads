afab8204c0877c8dfd83dc44a2bb5038f17a3b81
cimbriano <Christopher.Imbriano@gmail.com>
2012-12-17 23:46:29 -0500

    Clarified Ruby multiplication

--- benchmark/multiply.rb
+++ benchmark/multiply.rb
@@ -1,22 +1,16 @@
 require 'benchmark'
 require 'prover'
 
-puts "Benchmarking time to do matrix multiplications"
-Benchmark.bm(10) do |x|
+puts "Benchmarking time to do Ruby matrix multiplications"
+Benchmark.bm(7) do |x|
 
-	(10..100).step(10) do |n|
-		(10..100).step(10) do |k|
+	(10..20).step(10) do |n|
+		(10..20).step(10) do |k|
 
 			p = Prover.new(k,n,16)
 			v = Vector.elements( Array.new(p.k * p.log_q_ceil) { rand p.q })
 
-			x.report("Matrix Mulp --  k:#{k} n:#{n}"){ 100.times { p.L * v } }
+			x.report("k:#{k} n:#{n}"){ 100.times { p.L * v } }
 		end
 	end
-
-
-
-
-
-
 end
