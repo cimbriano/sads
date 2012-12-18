3b9167b9c9e2678036d36a0d1e9f39b5e4166022
cimbriano <Christopher.Imbriano@gmail.com>
2012-12-18 10:51:03 -0500

    Added files as place holder for various benchmarking tasks

---
+++ benchmark/generate_proof.rb
@@ -0,0 +1,8 @@
+require 'benchmark'
+require 'prover'
+
+puts "Benchmarking time for Prover to generate a proof"
+
+Benchmark.bm do |x|
+
+end
---
+++ benchmark/verify_proof.rb
@@ -0,0 +1,8 @@
+require 'benchmark'
+require 'verifier'
+
+puts "Benchmarking Verifier's verify proof operation"
+
+Benchmark.bm do |x|
+
+end
