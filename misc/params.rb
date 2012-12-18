cf92e9e16888661d815e7f78824b405b17b1c63e
cimbriano <Christopher.Imbriano@gmail.com>
2012-12-18 18:52:13 -0500

    Fixed up params to format data for report (and use prover class since there's no Sads class anymore)

--- misc/params.rb
+++ misc/params.rb
@@ -1,15 +1,17 @@
-require 'sads'
-s = Sads.new(0,0,0,true)
+require 'prover'
+p = Prover.new(0,0,0,true)
 f = File.open('./misc/params_bigger_steps.txt', 'w')
 
 
-f.write("k\t\t\t\tn\t\t\t\tq\t\t\t\t\tL/R dim\t\t\tEstimated Size of L or R\n")
+f.write("k\t\tn\t\tq\t\tlog_q\t\tbytes_per_entry\t\tL/R dim\t\t\tEstimated Size of L or R\n")
 
-(100..10000).step(1000) do |k|
-	(1000..10000000).step(1000000) do |n|
-		q = s.calculate_q(k, n)
+[100_000].each do |n|
+
+	(1000..10000000).step(1000) do |k|
+		q = p.calculate_q(k, n)
+		log_q = Math.log2(q).ceil
 		l_rows = k
-		l_cols = s.calculate_mu(k, q) / 2
+		l_cols = p.calculate_mu(k, q) / 2
 		l_num_elements = l_rows * l_cols
 
 		# Assuming 4 bytes per integer
@@ -17,12 +19,19 @@ f.write("k\t\t\t\tn\t\t\t\tq\t\t\t\t\tL/R dim\t\t\tEstimated Size of L or R\n")
 
 		# Each element in L can be a number up to q,
 		# 		which require the following number of bytes
-		size_per_element = Math.log2(q).ceil / 8.0
+		bytes_per_entry = (log_q / 8.0).ceil
 
 		# 1048576 bytes per megabyte
-		l_data_size = size_per_element * l_num_elements / 1048576
+		l_data_size = bytes_per_entry * l_num_elements / 1048576
+		tabs = "\t" *  ((10 - Math.log10(n)) / 2)
 
-		f.write("#{k}\t\t#{n}\t\t#{q}\t\t#{l_rows} x #{l_cols}\t\t#{'%.0f' % l_data_size}MB\n")
+		# line = [k, n, q, log_q, bytes_per_entry, "#{l_rows} x #{l_cols}", "#{'%.0f' % l_data_size}MB"].join(tabs)
+
+		line = [k, n, "#{l_rows} x #{l_cols}", "#{'%.0f' % l_data_size}MB"].join('&')
+		f.puts line + '\\\\'
+
+		# f.write("#{k}\t#{n}\t#{q}\t#{l_rows} x #{l_cols}\t#{log_q}\t#{'%.0f' % l_data_size}MB\n")
+
 	end
 end
 
