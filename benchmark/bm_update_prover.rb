c99a29e592270b21b4e1b6c5de26655659f60703
cimbriano <Christopher.Imbriano@gmail.com>
2013-01-31 16:49:36 -0500

    Reseting the prover storage with saved references

--- benchmark/bm_update_prover.rb
+++ benchmark/bm_update_prover.rb
@@ -9,23 +9,30 @@ puts "Benchmarking time to update the prover with a new 'stream' element"
 	puts "k: #{k}"
 	(50...51).step(1) do |n|
 		puts "Stream Size: #{n}"
-		(8...9).step(1) do |m|
+		(200...1000).step(200) do |m|
 			puts "Universe Size: #{m}"
 			p = Prover.new(k, n, m)
 			# v = Verifier.new(p.k, p.stream_bound_n, p.q, p.log_q_ceil, p.L, p.R, p.universe_size_m)
 
+			# Pre-generate random elements to add to the prover
 			num_elements_to_add = 100
 			elements = Array.new(num_elements_to_add){ rand(p.universe_size_m) }
+
+			# Obtain references to provers internal objects
+			labels = p.labels
+			p_labels = p.partial_labels
+			leaves = p.leaves
 
 
 			Benchmark.bmbm do |x|
 				(0...num_elements_to_add).each do |index|
-					x.report("k:#{k}, n:#{n}, m:#{m}, num:#{num_elements_to_add}") { p.addElement(elements[index]) }
+					x.report("k:#{k}, n:#{n}, m:#{m}, ele added:#{num_elements_to_add}") { p.addElement(elements[index]) }
 
-					# Reset prover
-					p.labels = {}
-					p.partial_labels = {}
-					p.leaves = {}
+					# Reset prover storage, outside of report
+					labels.clear
+					p_labels.clear
+					leaves.clear
+
 				end
 			end
 
